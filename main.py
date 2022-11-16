e003e3af12179daa4f6817b71c493c1df3428f93
Jorge Kimura <kimurajorge2001@gmail.com>
2022-11-16 15:00:56 -0300

    feat: iniciando projeto

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from management import product_handler
+
+def main():
+    product_handler.get_product_by_id(3)
+
 if __name__ == "__main__":
     # Seus prints de teste aqui
-    ...
+    main()
---
+++ management/product_handler.py
@@ -0,0 +1,8 @@
+from menu import products
+
+def get_product_by_id(id):
+
+    for item in products:
+        print(item)
+
+    return "olá"
